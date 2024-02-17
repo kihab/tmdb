8bab1eb0da72754822b11ecff88a58a52e1bd9cb
Karim Ihab <karim.ihab@gmail.com>
2024-02-17 22:17:56 +0100

    Refactor the MovieService File.

---
+++ TMDB/TMDB/Models/MovieResponse.swift
@@ -0,0 +1,10 @@
+//
+//  MovieResponse.swift
+//  TMDB
+//
+//  Created by Karim Ihab on 17/02/2024.
+//
+
+struct MovieResponse: Codable {
+    let results: [Movie]
+}
--- TMDB/TMDB/NetworkServices/MovieService.swift
+++ TMDB/TMDB/NetworkServices/MovieService.swift
@@ -9,6 +9,21 @@ import Foundation
 
 struct MovieService: MovieServiceProtocol {
     
+    private func performRequest<T: Decodable>(url: URL, decodingType: T.Type) async throws -> T {
+        var request = URLRequest(url: url, timeoutInterval: 10.0)
+        request.addValue("Bearer \(Constants.bearerToken)", forHTTPHeaderField: "Authorization")
+        request.httpMethod = "GET"
+        
+        let (data, response) = try await URLSession.shared.data(for: request)
+        
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            let statusCode = (response as? HTTPURLResponse)?.statusCode ?? -1
+            throw NSError(domain: "Error fetching data", code: statusCode, userInfo: [NSLocalizedDescriptionKey: "Failed with status code \(statusCode)"])
+        }
+        
+        return try JSONDecoder().decode(decodingType, from: data)
+    }
+
     func fetchTrendingMovies(page: Int) async throws -> [Movie] {
         guard let url = Constants.baseURL?.appendingPathComponent("discover/movie"),
               var components = URLComponents(url: url, resolvingAgainstBaseURL: true) else {
@@ -24,18 +39,7 @@ struct MovieService: MovieServiceProtocol {
             throw NSError(domain: "Invalid URL", code: -1, userInfo: nil)
         }
         
-        var request = URLRequest(url: finalURL, timeoutInterval: 10.0)
-        request.addValue("Bearer \(Constants.bearerToken)", forHTTPHeaderField: "Authorization")
-        request.httpMethod = "GET"
-        
-        let (data, response) = try await URLSession.shared.data(for: request)
-        
-        guard (response as? HTTPURLResponse)?.statusCode == 200 else {
-            throw NSError(domain: "Error fetching movies", code: -1, userInfo: nil)
-        }
-        
-        
-        let decodedResponse = try JSONDecoder().decode(MovieResponse.self, from: data)
+        let decodedResponse: MovieResponse = try await performRequest(url: finalURL, decodingType: MovieResponse.self)
         return decodedResponse.results
     }
     
@@ -44,31 +48,6 @@ struct MovieService: MovieServiceProtocol {
             throw NSError(domain: "Invalid URL", code: -1, userInfo: nil)
         }
         
-        var request = URLRequest(url: url, timeoutInterval: 10.0)
-        request.httpMethod = "GET"
-        request.addValue("Bearer \(Constants.bearerToken)", forHTTPHeaderField: "Authorization")
-        
-        do {
-            let (data, response) = try await URLSession.shared.data(for: request)
-            
-            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
-                let statusCode = (response as? HTTPURLResponse)?.statusCode ?? -1
-                throw NSError(domain: "Error fetching movie details", code: statusCode, userInfo: [NSLocalizedDescriptionKey: "Failed with status code \(statusCode)"])
-            }
-            
-            return try JSONDecoder().decode(MovieDetails.self, from: data)
-        } catch let jsonError as DecodingError {
-            // This provides more insight into what went wrong during decoding
-            print("Decoding error: \(jsonError.localizedDescription)")
-            throw jsonError
-        } catch {
-            // For other errors like network issues
-            print("Unexpected error: \(error.localizedDescription)")
-            throw error
-        }
+        return try await performRequest(url: url, decodingType: MovieDetails.self)
     }
-}
-
-struct MovieResponse: Codable {
-    let results: [Movie]
 }
